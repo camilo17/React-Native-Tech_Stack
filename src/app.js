19206002658b62c6f94b936bb5f0d9da895f0ec5
Camilo Guerrero <cami172013@gmail.com>
2017-01-05 11:00:28 -0600

    Added header component

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
 import reducers from './reducers';
 
+import {Header} from './components/common';
+
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
             <View>
-
+                <Header headerText="Tech Stack"/>
             </View>
         </Provider>
     );
